8ffcbf9548f1a88c0c505157ba4eb88917a590e0
obarker94 <me@oliverbarker.io>
2023-10-12 05:03:56 +0100

    last update

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,6 +2,6 @@ import { Express, Request, Response } from 'express'
 
 export function index(app: Express) {
   app.get('/', (_req: Request, res: Response) => {
-    res.send('yeeee boy')
+    res.send('doing an update')
   })
 }
