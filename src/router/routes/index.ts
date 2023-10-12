ad2052a81eaa21d1494fa88f1daf64329e4d7f3c
obarker94 <me@oliverbarker.io>
2023-10-12 19:50:05 +0100

    example commit

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,6 +2,6 @@ import { Express, Request, Response } from 'express'
 
 export function index(app: Express) {
   app.get('/', (_req: Request, res: Response) => {
-    res.send('original app - ooo')
+    res.send('this is a great commit')
   })
 }
