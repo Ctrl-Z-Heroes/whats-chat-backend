a1ee7895be21da7aeb568b9c6a7121770d1d149a
obarker94 <me@oliverbarker.io>
2023-10-12 18:37:05 +0100

    test

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,6 +2,6 @@ import { Express, Request, Response } from 'express'
 
 export function index(app: Express) {
   app.get('/', (_req: Request, res: Response) => {
-    res.send('test new update thing yeaaaa')
+    res.send('original app - ooo')
   })
 }
